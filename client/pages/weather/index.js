633744814e5ab6f60a45dfeb6b3721dc5a636b6c
MeloGuo <guoziliang199606@gmail.com>
2018-11-19 14:55:02 +0800

    feat(weather): weather首页基本样式

--- client/pages/weather/index.js
+++ client/pages/weather/index.js
@@ -9,9 +9,45 @@ import { test } from '../../lib/api'
 /* eslint-disable spaced-comment */
 
 Page({
-  onClickTitle () {
-    test(1, 2).then((result) => {
-      console.log(result)
+  data: {
+    statusBarHeight: 32,
+    backgroundImage: '../../images/cloud.jpg',
+    backgroundColor: '#62aadc',
+    current: {
+      temp: '0',
+      weather: '数据获取中',
+      humidity: '1',
+      icon: 'xiaolian'
+    },
+    today: {
+      temp: 'N/A',
+      weather: '暂无'
+    },
+    tomorrow: {
+      temp: 'N/A',
+      weather: '暂无'
+    },
+    hourlyData: [],
+    city: '北京',
+    weeklyData: [],
+    width: 375,
+    scale: 1,
+    address: '定位中',
+    lat: 40.056974,
+    lon: 116.307689
+  },
+
+  onLoad () {
+    this.setPaddingTop()
+  },
+
+  setPaddingTop () {
+    wx.getSystemInfo({
+      success: (result) => {
+        this.setData({
+          paddingTop: result.statusBarHeight + 12
+        })
+      }
     })
   }
 })
