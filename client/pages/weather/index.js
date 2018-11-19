644ff72ee567a6943a76cd79ea0e77a61186cc27
MeloGuo <guoziliang199606@gmail.com>
2018-11-19 21:08:28 +0800

    feat: 增加下拉刷新

--- client/pages/weather/index.js
+++ client/pages/weather/index.js
@@ -43,6 +43,12 @@ Page({
     this.getLocation()
   },
 
+  onPullDownRefresh () {
+    this.getWeatherData(() => {
+      wx.stopPullDownRefresh()
+    })
+  },
+
   setPaddingTop () {
     wx.getSystemInfo({
       success: (result) => {
